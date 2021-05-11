fd32c76d678e8e8dcd310fb628df8c1d14d7dd66
n-fridman <n.fridman@goto-apps.com>
2021-05-11 04:36:22 +0300

    Add score manager

---
+++ Assets/Scripts/DarkenDinosaur/Managers/ScoreManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DarkenDinosaur.Managers
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        [Header("Settings")] 
+        [SerializeField] private int addScorePerIteration;
+        [SerializeField] private float iterationDelta;
+        [SerializeField] private float minIterationDelta;
+        [SerializeField] private float maxIterationDelta;
+        [SerializeField] private float lessDeltaPerSecond;
+        [SerializeField] private float nonLessDeltaTime;
+        [Space] 
+        [SerializeField] private int scoreCount;
+        [SerializeField] private int highScoreCount;
+
+        [SerializeField] private UnityEvent<int> scoreChanged;
+        [SerializeField] private UnityEvent<int> highScoreChanged;
+        
+        private IEnumerator ScoreCounter()
+        {
+            while (true)
+            {
+                this.scoreCount += this.addScorePerIteration;
+                this.scoreChanged?.Invoke(this.scoreCount);
+
+                if (this.scoreCount > this.highScoreCount)
+                {
+                    this.highScoreCount = this.scoreCount;
+                    this.highScoreChanged?.Invoke(this.highScoreCount);
+                }
+
+                yield return new WaitForSeconds(this.iterationDelta);
+            }
+        }
+
+        private IEnumerator IterationDeltaCounter()
+        {
+            yield return new WaitForSeconds(this.nonLessDeltaTime);
+            while (true)
+            {
+                this.iterationDelta -= this.lessDeltaPerSecond / 10;
+                this.iterationDelta = Mathf.Clamp(this.iterationDelta, this.minIterationDelta, this.maxIterationDelta);
+
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        
+        /// <summary>
+        /// Game start event handler.
+        /// </summary>
+        public void OnGameStart()
+        {
+            StartCoroutine(ScoreCounter());
+            StartCoroutine(IterationDeltaCounter());
+        }
+        
+        /// <summary>
+        /// Player dead event handler,
+        /// </summary>
+        public void OnPlayerDead()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
