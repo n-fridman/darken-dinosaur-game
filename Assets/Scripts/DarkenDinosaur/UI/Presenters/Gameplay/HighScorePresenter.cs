9c2b21ae4dd76712ba88c0cc91a78342d237f540
n-fridman <n.fridman@goto-apps.com>
2021-10-04 04:31:00 +0300

    Update High score presenter

--- Assets/Scripts/DarkenDinosaur/UI/Presenters/Gameplay/HighScorePresenter.cs
+++ Assets/Scripts/DarkenDinosaur/UI/Presenters/Gameplay/HighScorePresenter.cs
@@ -2,42 +2,43 @@ using DarkenDinosaur.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace DarkenDinosaur.UI
+namespace DarkenDinosaur.UI.Presenters.Gameplay
 {
+    [AddComponentMenu("Darken Dinosaur/UI/Presenters/Gameplay/High Score Presenter")]
     [RequireComponent(typeof(Text))]
     public class HighScorePresenter : MonoBehaviour
     {
         [Header("Components")]
-        [SerializeField] private Text highScoreText;
+        [SerializeField] private Text _highScoreText;
 
         [Header("Settings")] 
-        [SerializeField] private string prefix;
+        [SerializeField] private string _prefix;
         
         private void Awake()
         {
-            if (this.highScoreText == null) this.highScoreText = GetComponent<Text>();
+            if (_highScoreText == null) _highScoreText = GetComponent<Text>();
         }
         
         /// <summary>
         /// Data loaded event handler.
         /// </summary>
         /// <param name="data">Game data.</param>
-        public void OnDataLoaded(GameData data) => this.highScoreText.text = $"{this.prefix}{data.highScoreCount:0000}";
+        public void OnDataLoaded(GameData data) => _highScoreText.text = $"{_prefix}{data.highScoreCount:0000}";
         
         /// <summary>
         /// On high score changed event handler.
         /// </summary>
         /// <param name="highScore">High score count.</param>
-        public void OnHighScoreChanged(int highScore) => this.highScoreText.text = $"{this.prefix}" + highScore.ToString("0000");
+        public void OnHighScoreChanged(int highScore) => _highScoreText.text = $"{_prefix}" + highScore.ToString("0000");
         
         /// <summary>
         /// Hide high score counter.
         /// </summary>
-        public void HideHighScore() => this.gameObject.SetActive(false);
+        public void HideHighScore() => gameObject.SetActive(false);
         
         /// <summary>
         /// Show high score counter.
         /// </summary>
-        public void ShopHighScore() => this.gameObject.SetActive(true);
+        public void ShopHighScore() => gameObject.SetActive(true);
     }
 }
